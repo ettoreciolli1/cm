c3cbe5580c342640cdbb0b1561aceed755f8afe4
Ettore Ciolli <ettorecoding@gmail.com>
2025-10-31 16:02:11 +0100

    ready

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, { ssl: "require" });
+export const db = drizzle(client);
